25712bf54fcd1b954b38c8824011f51da43064bd
Mário Ponte <ponteolavo30@gmail.com>
2022-11-10 13:34:41 +0000

    edit

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
 import ColorModeProvider, { ColorModeContext } from "../src/components/Menu/components/ColorMode";
+import RegisterVideo from "../src/components/RegisterVideo";
 
 const theme = {
     light: {
@@ -32,7 +33,7 @@ function ProviderWrapper(props) {
     )
 }
 
-function MyApp({ Component, pageProps }) {
+function Root({ Component, pageProps }) {
     const contexto = React.useContext(ColorModeContext);
     console.log(contexto.mode);
 
@@ -40,6 +41,7 @@ function MyApp({ Component, pageProps }) {
             <ThemeProvider theme={theme[contexto.mode]}>
                 <CSSReset />
                 <Component {...pageProps} />
+                <RegisterVideo />
             </ThemeProvider>
     )
 }
@@ -47,7 +49,7 @@ function MyApp({ Component, pageProps }) {
 export default function _App(props) {
     return (
         <ProviderWrapper>
-            <MyApp {...props} />
+            <Root {...props} />
         </ProviderWrapper>
     )
 };
